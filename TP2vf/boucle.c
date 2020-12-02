7f341589e7569d0ca5cabfacfc6aa8e0acd1553e
TrehoutA <71724142+TrehoutA@users.noreply.github.com>
2020-12-02 08:48:17 +0100

    Update boucle.c

--- TP2vf/boucle.c
+++ TP2vf/boucle.c
@@ -4,15 +4,15 @@
 void main()
 {
 	int n = 3;
-	unsigned short somme; 
-
+	unsigned short somme; // max 65 535
+	// boucle for
 	somme = 0;
 	for (int i = 1; i <= n; i++)
 	{
 		somme += i;
 	}
 	printf("FOR- Somme des %d premiers entiers : %d\n", n, somme);
-
+	// boucle while
 	somme = 0;
 	int p = 1;
 	while (p <= n)
@@ -21,7 +21,7 @@ void main()
 		p += 1;
 	}
 	printf("WHILE- Somme des %d premiers entiers : %d\n", n, somme);
-
+	// boucle do while
 	somme = 0;
 	p = 1;
 	do {
@@ -31,9 +31,9 @@ void main()
 	printf("DO...WHILE- Somme des %d premiers entiers : %d\n", n, somme);
 
 
-	/******* VALEUR MAXIMALE DE N SANS DEPASSEMENT DE SOMME ******
-	* Il faut resoude n(n+1)/2 <= 65 535
-	* On trouve Nmax = 361 */
+	//Valeur max n sans dépassement de somme
+	// Résolution de n(n+1)/2 <= 65 535
+	// Ainsi Nmax = 361 
 
 	const int N = 362;
 	somme = 0;
@@ -49,7 +49,7 @@ void main()
 	}
 	printf("WHILE MAX- Somme des %d premiers entiers : %d\n", N, somme);
 
-
+	// Demander la valeur n à l'utilisateur et recommencer
 	somme = 0;
 	scanf_s("%d", &n);
 	while (n > 361 || n < 1)
